4acf9ed25676b3b641e0de547307a0331f0b0b25
AndreasDuma <46629634+AndreasDuma@users.noreply.github.com>
2019-11-17 12:26:54 -0500

    Update Shooter.java

--- src/main/java/org/longmetal/Shooter.java
+++ src/main/java/org/longmetal/Shooter.java
@@ -5,16 +5,14 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Shooter
 {
-    private Spark shooterL, shooterR;
+    private Spark shooterL, shooterR, singulator;
     private TalonSRX angle;
     public Shooter()
     {
         angle = new TalonSRX(5);
         shooterL = new Spark(1);
         shooterR = new Spark(2);
+        singulator = new Spark(3);
     }
-
-    
+   
 }
- 
-
